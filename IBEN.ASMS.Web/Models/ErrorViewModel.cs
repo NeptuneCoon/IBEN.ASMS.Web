14324342dd5228953011975a95580cea498a4eb0
gao <mercury1259@outlook.com>
2023-05-07 22:31:37 +0800

    初始化项目

---
+++ IBEN.ASMS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IBEN.ASMS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
